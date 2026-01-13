4314a172f6f0a786bf373884f685cb0864403cf5
Prasad Kakde <kakdeprasad11@gmail.com>
2026-01-13 21:10:32 +0530

    data base connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err.message);
+  } catch (error) {
+    console.error(`❌ MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
